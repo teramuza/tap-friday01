42e2f4b859592389f17c4fbccba434037a103243
Teuku Raja M Z <teramuza@gmail.com>
2021-01-15 21:41:54 +0700

    integrate redux with homescreen

--- src/Screens/Screen.config.js
+++ src/Screens/Screen.config.js
@@ -1,4 +1,4 @@
-import HomeScreen from './Home/Home.screen.component';
+import HomeScreen from './Home/Home.container';
 import DetailScreen from './Detail/Detail.screen.component';
 
 export {HomeScreen, DetailScreen};
